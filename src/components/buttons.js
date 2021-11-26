1dd361a8278df72e44df62a4b73ec60b79e3e246
perledecoco <mahelaplt0801@gmail.com>
2021-11-26 10:57:00 +0100

    fonctionnalité point

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom'
+
 import ReactDOM from 'react-dom';
 
 function Buttons() {
@@ -14,15 +14,18 @@ function Buttons() {
     //ReactDOM.render(stop_btn, document.querySelector('#stop'))
 
     const [startBtn, setStartBtn] = useState('LANCER')
-    const [count, setCount] = useState(0)
-    const [InputTime, setInputTime] = useState(0)
+    //
+    const [count, setCount] = useState(0) //décompte//
+    const [InputTime, setInputTime] = useState(0) //temps que l'utilisateur rentre//
+    const [point, setPoint] = useState(0)
+ 
 
 
     const GetInputTime = (event) => {
         setInputTime((event.target.value))
-       
+
         setCount(InputTime)
-        
+        setPoint(InputTime * 0.5)
     }
 
 
@@ -30,11 +33,20 @@ function Buttons() {
         // executé après le premier rendu et à chaque changement des variables entre crochets....****
 
         if (startBtn == 'STOP') {
+
+
+
             const interval = setTimeout(() => {
                 setInputTime(InputTime - 1)
                 setCount(count - 1)
                 document.getElementById('timeInput').disabled = true;
+
+
+                console.log(point)
+
             }, 1000);
+
+
             if (startBtn == 'LANCER') {
                 document.getElementById('timeInput').disabled = false;
 
@@ -43,32 +55,37 @@ function Buttons() {
 
                 clearTimeout(interval)
             }
+
+            if (count == 0) {
+                
+            }
         }
-            // lorsque ces variables entre cochets changent la fonction useEffect sera appellée *****
-        }, [count,startBtn]);
+        // lorsque ces variables entre cochets changent la fonction useEffect sera appellée *****
+    }, [count, startBtn]);
 
 
 
-const changeValue = () => {
-    console.log(startBtn)
-    if (startBtn == 'LANCER') {
-        setStartBtn('STOP')
+    const changeValue = () => {
+        console.log(startBtn)
+        if (startBtn == 'LANCER') {
+            setStartBtn('STOP')
+        }
+        else if (startBtn == 'STOP') {
+            setStartBtn('LANCER')
+
+        }
+
     }
-    else if (startBtn == 'STOP') {
-        setStartBtn('LANCER')
 
-    }
+    return (
 
-}
-
-return (
-
-    <div>
-        <input min="-1" max="61" step="1" onChange={GetInputTime} id="timeInput" type="range" value={InputTime}></input>
-        <div class="time"><h1>Temps :</h1>{count}</div>
-        <button onClick={changeValue}>{startBtn}</button>
-    </div>
-)
+        <div>
+            <input min="-1" max="61" step="1" onChange={GetInputTime} id="timeInput" type="range" value={InputTime}></input>
+            <div class="time"><h1>Temps :</h1>{count}</div>
+            <button onClick={changeValue}>{startBtn}</button>
+            <div style={{display:'none'}} id="score"><h1> bravo ! tu as gagné {point}</h1></div>
+        </div>
+    )
 
 }
 
